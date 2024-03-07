08701a0977f863ba69f785cb6db0fdd1da50c1f6
David <floresdavid126@gmail.com>
2024-03-06 18:00:01 -0600

    First commit

---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'SEO Title',
+    description: 'SEO Description',
+};
+export default function ContactPage () {
+
+    return (
+        <>
+            <span className ='text-5xl'>Página de contacto</span>
+        </>
+    )
+}
---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'SEO Title',
+    description: 'SEO Description',
+};
+export default function PricingPage () {
+
+    return (
+        <>
+            <span className = 'text-5xl'>Esta es la página de pricing</span>
+        </>
+    )
+}
